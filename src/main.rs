9bdad54c7192c4e777f8879714f3e9b7f7174db7
jw910731 <jw910731@gmail.com>
2024-09-06 16:55:05 +0800

    Add ray marching based ray intersection

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,132 @@
-mod ray;
 mod geometry;
+mod ray_marching;
+mod scene;
+
+use std::{env::args, fs::File, io::Read};
+
+use geometry::{RayMarchable, Sphere, Triangle};
+use glam::{vec3, Vec3};
+
+use anyhow::{Error, Result};
+use scene::Scene;
+
+struct InputData {
+    eye: Vec3,
+    img_coord: [Vec3; 4], // UL, UR, LL, LR
+    resolution: (i32, i32),
+    objects: Vec<Box<dyn RayMarchable>>,
+}
+
+impl InputData {
+    fn parse(input_text: &str) -> Result<InputData> {
+        let mut ret = InputData {
+            eye: vec3(0f32, 0f32, 0f32),
+            img_coord: [
+                vec3(0f32, 0f32, 0f32),
+                vec3(0f32, 0f32, 0f32),
+                vec3(0f32, 0f32, 0f32),
+                vec3(0f32, 0f32, 0f32),
+            ],
+            resolution: (0, 0),
+            objects: vec![],
+        };
+        let lines = input_text.lines();
+        let parse_error = || Error::msg("Parse error");
+        for line in lines {
+            let mut sep = line.split_ascii_whitespace();
+            let id = sep.next().unwrap();
+            match id {
+                "E" => {
+                    ret.eye = vec3(
+                        sep.next().ok_or(parse_error())?.parse()?,
+                        sep.next().ok_or(parse_error())?.parse()?,
+                        sep.next().ok_or(parse_error())?.parse()?,
+                    );
+                }
+                "O" => {
+                    ret.img_coord = [
+                        vec3(
+                            sep.next().ok_or(parse_error())?.parse()?,
+                            sep.next().ok_or(parse_error())?.parse()?,
+                            sep.next().ok_or(parse_error())?.parse()?,
+                        ),
+                        vec3(
+                            sep.next().ok_or(parse_error())?.parse()?,
+                            sep.next().ok_or(parse_error())?.parse()?,
+                            sep.next().ok_or(parse_error())?.parse()?,
+                        ),
+                        vec3(
+                            sep.next().ok_or(parse_error())?.parse()?,
+                            sep.next().ok_or(parse_error())?.parse()?,
+                            sep.next().ok_or(parse_error())?.parse()?,
+                        ),
+                        vec3(
+                            sep.next().ok_or(parse_error())?.parse()?,
+                            sep.next().ok_or(parse_error())?.parse()?,
+                            sep.next().ok_or(parse_error())?.parse()?,
+                        ),
+                    ];
+                }
+                "R" => {
+                    ret.resolution = (
+                        sep.next().ok_or(parse_error())?.parse()?,
+                        sep.next().ok_or(parse_error())?.parse()?,
+                    );
+                }
+                "S" => {
+                    ret.objects.push(Box::new(Sphere::new(
+                        vec3(
+                            sep.next().ok_or(parse_error())?.parse()?,
+                            sep.next().ok_or(parse_error())?.parse()?,
+                            sep.next().ok_or(parse_error())?.parse()?,
+                        ),
+                        sep.next().ok_or(parse_error())?.parse()?,
+                    )));
+                }
+                "T" => {
+                    ret.objects.push(Box::new(Triangle::new([
+                        vec3(
+                            sep.next().ok_or(parse_error())?.parse()?,
+                            sep.next().ok_or(parse_error())?.parse()?,
+                            sep.next().ok_or(parse_error())?.parse()?,
+                        ),
+                        vec3(
+                            sep.next().ok_or(parse_error())?.parse()?,
+                            sep.next().ok_or(parse_error())?.parse()?,
+                            sep.next().ok_or(parse_error())?.parse()?,
+                        ),
+                        vec3(
+                            sep.next().ok_or(parse_error())?.parse()?,
+                            sep.next().ok_or(parse_error())?.parse()?,
+                            sep.next().ok_or(parse_error())?.parse()?,
+                        ),
+                    ])));
+                }
+                _ => {}
+            }
+        }
+        Ok(ret)
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+    let args = args().collect::<Vec<_>>();
+    if args.len() < 3 {
+        eprintln!("You should provide 1 argument to specify the path of input file");
+        eprintln!("You should provide 2 argument to specify the path of output file");
+        return;
+    }
+    let mut in_file = File::open(args[1].as_str()).unwrap();
+    let mut out_file = File::create(args[2].as_str()).unwrap();
+    let mut input_buf = String::new();
+    in_file.read_to_string(&mut input_buf).unwrap();
+    let data = InputData::parse(&input_buf).unwrap();
+    let scene = Scene {
+        eye: data.eye,
+        img_coord: data.img_coord,
+        resolution: data.resolution,
+        scene_obj: data.objects,
+        background: vec3(0f32, 0f32, 0f32),
+    };
+    scene.render(&mut out_file).unwrap();
 }
---
+++ src/scene.rs
@@ -0,0 +1,67 @@
+use core::f32;
+use std::{fmt::format, fs::File, io::Write};
+
+use anyhow::Result;
+use glam::Vec3;
+
+use crate::{
+    geometry::{Ray, RayMarchable},
+    ray_marching::ray_marching,
+};
+
+pub struct Scene {
+    pub eye: Vec3,
+    pub img_coord: [Vec3; 4], // UL, UR, LL, LR
+    pub resolution: (i32, i32),
+    pub scene_obj: Vec<Box<dyn RayMarchable>>,
+    pub background: Vec3,
+}
+
+fn output_util(vec: Vec3, file: &mut File) -> Result<()> {
+    let collision = vec;
+    let results = (255.0 * (collision + 1.0) / 2.0).round();
+    file.write(
+        format!(
+            "{} {} {} ",
+            results.x as i32, results.y as i32, results.z as i32
+        )
+        .as_bytes(),
+    )?;
+    Ok(())
+}
+
+impl Scene {
+    pub fn render(&self, file: &mut File) -> Result<()> {
+        file.write("P3\n".as_bytes())?;
+        file.write(format!("{} {}\n255\n", self.resolution.0, self.resolution.1).as_bytes())?;
+        let (wv, hv) = (
+            self.img_coord[1] - self.img_coord[0],
+            self.img_coord[2] - self.img_coord[0],
+        );
+        let world_pixel_width = wv.length() / self.resolution.0 as f32;
+        let world_pixel_height = hv.length() / self.resolution.1 as f32;
+        let origin = self.img_coord[0]
+            + (wv.normalize() * world_pixel_width / 2.0)
+            + (hv.normalize() * world_pixel_height / 2.0);
+        for r in 0..self.resolution.1 {
+            for c in 0..self.resolution.0 {
+                let world_canvas_coord = origin
+                    + wv.normalize() * world_pixel_width * (c as f32)
+                    + hv.normalize() * world_pixel_height * (r as f32);
+                let ray = Ray::new(world_canvas_coord, world_canvas_coord - self.eye);
+                let lerp = self.scene_obj.iter().fold(f32::INFINITY, |acc, obj| {
+                    let collision = ray_marching(ray, obj.as_ref());
+                    let t = collision.map(|e| ray.solve(e));
+                    t.map(|t| acc.min(t)).unwrap_or(acc)
+                });
+                if lerp.is_finite() {
+                    output_util(ray.lerp(lerp), file)?;
+                } else {
+                    output_util(self.background, file)?;
+                };
+            }
+            file.write("\n".as_bytes())?;
+        }
+        Ok(())
+    }
+}

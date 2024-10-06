8db711d03982ae6b1370d539ad02a43a476d65c6
jw910731 <jw910731@gmail.com>
2024-10-07 04:14:05 +0800

    Reconsturct build script

--- build.rs
+++ build.rs
@@ -1,8 +1,16 @@
 use spirv_builder::{MetadataPrintout, SpirvBuilder};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    SpirvBuilder::new("kernels", "spirv-unknown-vulkan1.1")
-        .print_metadata(MetadataPrintout::None)
-        .build()?;
-    Ok(())
+    println!("cargo:rerun-if-env-changed=RUST_ANALYZER_CHECK");
+    let result = SpirvBuilder::new("kernels", "spirv-unknown-spv1.3")
+        .print_metadata(MetadataPrintout::Full)
+        .build();
+    if std::env::var("RUST_ANALYZER_CHECK").map(|env| env == "1").unwrap_or_default() {
+        if let Err(msg) = result.map_err(|e| e.to_string()) {
+            println!("cargo:warning={}", msg);
+        }
+        Ok(())
+    } else {
+        result.map(|_| ()).map_err(|e| Box::from(e))
+    }
 }
